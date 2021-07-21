3c3a92435fe262b33787f80a160068c49cc11e31
Amjad Dabi <amjada@longleaf-login2.its.unc.edu>
2021-07-21 15:50:23 -0400

    changed selection coefficients

--- graphs.R
+++ graphs.R
@@ -1,15 +1,16 @@
 library(tidyverse)
-df = read.csv('/pine/scr/a/m/amjada/eff_sim/metrics_100k.csv')
+df = read.csv('/pine/scr/a/m/amjada/eff_sim/metrics_100.csv')
 
 df_mean <- 
   df %>% 
   group_by(generation) %>% 
-  summarise(mean_m1 = mean(m1_count)*10, mean_m2 = mean(m2_count),
-            mean_m3 = mean(m3_count), mean_rf = mean(mean_fitness)
-  )
+  summarise(mean_m1 = median(m1_count)*10, mean_m2 = median(m2_count),
+            mean_m3 = mean(m3_count), mean_rf = median(mean_fitness)
+  ) 
 
 ggplot(data=df_mean, aes(x=generation)) + 
-  geom_point(aes(y=mean_rf), color = 'red', size=0.05)
+  geom_line(aes(y=mean_rf), color = 'red', size=0.05) + 
+  labs(title = "Relative fitness 100")
 
-ggplot(data=df_mean, aes(x=generation)) + 
-  geom_point(aes(y=mean_m1), color = 'blue', size=0.05)
+# ggplot(data=df_mean, aes(x=generation)) + 
+#   geom_line(aes(y=mean_m1), color = 'blue', size=0.05)
